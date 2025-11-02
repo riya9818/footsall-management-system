f8fe664ff97661417b895eeb8ab587e9a1da5f23
riya9818 <reeyabasnet999@gmail.com>
2025-11-02 21:15:30 +0545

    hooking up the urls in each app plus updated views

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def users_home(request):
+def dashboard_home(request):
     return HttpResponse("Dashboard app working!")
