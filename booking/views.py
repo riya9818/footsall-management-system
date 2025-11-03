4da4e978c5a572b8fdd59f5eb46048372de8c02d
riya9818 <reeyabasnet999@gmail.com>
2025-11-03 20:56:05 +0545

    booking voews updated to create and view bookings

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import BookingForm
+from .models import Booking
 
-def booking_home(request):
-    return HttpResponse("Booking app working!")
+def booking_create(request):
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.booked_by = request.user  # user must be logged in
+            booking.save()
+            return redirect('booking_list')
+    else:
+        form = BookingForm()
+    return render(request, 'booking/booking_form.html', {'form': form})
+
+def booking_list(request):
+    bookings = Booking.objects.all().order_by('-date', 'start_time')
+    return render(request, 'booking/booking_list.html', {'bookings': bookings})
