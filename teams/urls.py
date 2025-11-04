5c546248c0cba2fbc6aa342f34a1ef032616f327
riya9818 <reeyabasnet999@gmail.com>
2025-11-04 21:21:57 +0545

    basic routes of team app

--- teams/urls.py
+++ teams/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.teams_home, name='teams_home'),
+    path('', views.team_list, name='team_list'),
+    path('add/', views.team_create, name='team_create'),
+    path('edit/<int:pk>/', views.team_edit, name='team_edit'),
+    path('delete/<int:pk>/', views.team_delete, name='team_delete'),
 ]
