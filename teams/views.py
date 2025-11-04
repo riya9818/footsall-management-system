2561494098d53b0aa8c8b14162e9a15f1e5cd392
riya9818 <reeyabasnet999@gmail.com>
2025-11-04 21:20:39 +0545

    Basic CRUD operations of views.py

--- teams/views.py
+++ teams/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Team
+from .forms import TeamForm
 
-def teams_home(request):
-    return HttpResponse("Teams app working!")
+def team_list(request):
+    teams = Team.objects.all()
+    return render(request, 'teams/team_list.html', {'teams': teams})
+
+def team_create(request):
+    if request.method == 'POST':
+        form = TeamForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('team_list')
+    else:
+        form = TeamForm()
+    return render(request, 'teams/team_form.html', {'form': form})
+
+def team_edit(request, pk):
+    team = get_object_or_404(Team, pk=pk)
+    if request.method == 'POST':
+        form = TeamForm(request.POST, instance=team)
+        if form.is_valid():
+            form.save()
+            return redirect('team_list')
+    else:
+        form = TeamForm(instance=team)
+    return render(request, 'teams/team_form.html', {'form': form})
+
+def team_delete(request, pk):
+    team = get_object_or_404(Team, pk=pk)
+    if request.method == 'POST':
+        team.delete()
+        return redirect('team_list')
+    return render(request, 'teams/team_confirm_delete.html', {'team': team})
