8d3c1877c554b3e71406defa57802610a22e9a61
riya9818 <reeyabasnet999@gmail.com>
2025-11-04 21:09:24 +0545

    login requried for booking

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('team', 'date', 'start_time', 'end_time', 'booked_by', 'created_at')
+    list_filter = ('date',)
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import BookingForm
 from .models import Booking
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def booking_create(request):
     if request.method == "POST":
         form = BookingForm(request.POST)
@@ -14,6 +16,7 @@ def booking_create(request):
         form = BookingForm()
     return render(request, 'booking/booking_form.html', {'form': form})
 
+@login_required
 def booking_list(request):
     bookings = Booking.objects.all().order_by('-date', 'start_time')
     return render(request, 'booking/booking_list.html', {'bookings': bookings})
