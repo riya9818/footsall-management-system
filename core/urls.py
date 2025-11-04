690cb0ee0492d2cac8bdad99a35049f31c1065a6
riya9818 <reeyabasnet999@gmail.com>
2025-11-04 21:12:39 +0545

    built in system auth in urls

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from main import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
@@ -26,6 +28,9 @@ urlpatterns = [
     path('teams/', include('teams.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('', include('main.urls')),  # route to main app
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+
 ]
 
 if settings.DEBUG:
