17a92179f86fc802bb1d67b2d621a5a5797d0633
riya9818 <reeyabasnet999@gmail.com>
2025-11-05 21:27:58 +0545

    forms.py updated for readibility

--- teams/forms.py
+++ teams/forms.py
@@ -1,12 +1,35 @@
+# from django import forms
+# from .models import Team
+
+# class TeamForm(forms.ModelForm):
+#     class Meta:
+#         model = Team
+#         fields = ['name', 'captain', 'members']
+#         widgets = {
+#             'name': forms.TextInput(attrs={'class': 'form-control'}),
+#             'captain': forms.Select(attrs={'class': 'form-select'}),
+#             'members': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+#         }
+
 from django import forms
 from .models import Team
 
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
-        fields = ['name', 'captain', 'members']
+        fields = ['name', 'captain']
+
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'captain': forms.Select(attrs={'class': 'form-select'}),
-            'members': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter team name'
+            }),
+            'captain': forms.Select(attrs={
+                'class': 'form-select'
+            }),
+        }
+
+        labels = {
+            'name': 'Team Name',
+            'captain': 'Team Captain',
         }
