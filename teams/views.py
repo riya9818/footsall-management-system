8b96097bf35c96c612761662944fcd27e2a8a6cc
riya9818 <reeyabasnet999@gmail.com>
2025-11-06 21:11:54 +0545

    updated to auth creation of teams

--- teams/views.py
+++ teams/views.py
@@ -6,16 +6,18 @@ def team_list(request):
     teams = Team.objects.all()
     return render(request, 'teams/team_list.html', {'teams': teams})
 
+@login_required
 def team_create(request):
     if request.method == 'POST':
         form = TeamForm(request.POST)
         if form.is_valid():
-            form.save()
+            team = form.save(commit=False)
+            team.captain = request.user  # Auto assign
+            team.save()
             return redirect('team_list')
     else:
         form = TeamForm()
     return render(request, 'teams/team_form.html', {'form': form})
-
 def team_edit(request, pk):
     team = get_object_or_404(Team, pk=pk)
     if request.method == 'POST':
