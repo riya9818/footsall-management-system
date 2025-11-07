66a174888ec67739bd22ba73f8c45aa1964ac96e
riya9818 <reeyabasnet999@gmail.com>
2025-11-07 21:01:28 +0545

    updated workflow of teams model

--- teams/models.py
+++ teams/models.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import User
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
-    captain = models.ForeignKey(User, on_delete=models.CASCADE)
-    members = models.TextField(help_text="Comma-separated list of team members", blank=True)
+    captain = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
