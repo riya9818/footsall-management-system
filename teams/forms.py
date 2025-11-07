8f954bc1f42e34e3d907121f045c898053e24774
riya9818 <reeyabasnet999@gmail.com>
2025-11-07 21:02:49 +0545

    upated views to restore captain field

--- teams/forms.py
+++ teams/forms.py
@@ -1,18 +1,6 @@
-# from django import forms
-# from .models import Team
-
-# class TeamForm(forms.ModelForm):
-#     class Meta:
-#         model = Team
-#         fields = ['name', 'captain', 'members']
-#         widgets = {
-#             'name': forms.TextInput(attrs={'class': 'form-control'}),
-#             'captain': forms.Select(attrs={'class': 'form-select'}),
-#             'members': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-#         }
-
 from django import forms
 from .models import Team
+from django.contrib.auth.models import User
 
 class TeamForm(forms.ModelForm):
     class Meta:
@@ -25,11 +13,11 @@ class TeamForm(forms.ModelForm):
                 'placeholder': 'Enter team name'
             }),
             'captain': forms.Select(attrs={
-                'class': 'form-select'
+                'class': 'form-select',
             }),
         }
 
         labels = {
             'name': 'Team Name',
-            'captain': 'Team Captain',
+            'captain': 'Select Captain',
         }
