8f676367f418717a8f844b41099ae26a18f79702
riya9818 <reeyabasnet999@gmail.com>
2025-11-07 21:15:40 +0545

    updated forms

--- teams/forms.py
+++ teams/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
-        fields = ['name', 'captain']
+        fields = ['name', 'captain', 'players']
 
         widgets = {
             'name': forms.TextInput(attrs={
@@ -15,9 +15,14 @@ class TeamForm(forms.ModelForm):
             'captain': forms.Select(attrs={
                 'class': 'form-select',
             }),
+            'players': forms.SelectMultiple(attrs={
+                'class': 'form-select',
+                'size': '6'
+            }),
         }
 
         labels = {
             'name': 'Team Name',
             'captain': 'Select Captain',
+            'players': 'Select Players',
         }
