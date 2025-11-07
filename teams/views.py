9a591426fc124105328cae095d5d9b5e12774796
riya9818 <reeyabasnet999@gmail.com>
2025-11-07 21:04:25 +0545

    upated views to restore captain

--- teams/views.py
+++ teams/views.py
@@ -7,18 +7,16 @@ def team_list(request):
     teams = Team.objects.all()
     return render(request, 'teams/team_list.html', {'teams': teams})
 
-@login_required
 def team_create(request):
     if request.method == 'POST':
         form = TeamForm(request.POST)
         if form.is_valid():
-            team = form.save(commit=False)
-            team.captain = request.user  # Auto assign
-            team.save()
+            form.save()
             return redirect('team_list')
     else:
         form = TeamForm()
     return render(request, 'teams/team_form.html', {'form': form})
+
 def team_edit(request, pk):
     team = get_object_or_404(Team, pk=pk)
     if request.method == 'POST':
