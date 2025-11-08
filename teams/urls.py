90dc3a44fe8d14f2d22329331ed89982db75d3a0
riya9818 <reeyabasnet999@gmail.com>
2025-11-08 21:30:02 +0545

    updated team urls

--- teams/urls.py
+++ teams/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('add/', views.team_create, name='team_create'),
     path('edit/<int:pk>/', views.team_edit, name='team_edit'),
     path('delete/<int:pk>/', views.team_delete, name='team_delete'),
+     path('<int:team_id>/', views.team_detail, name='team_detail'),
+    path('<int:team_id>/add_player/', views.add_player, name='add_player'),
 ]
