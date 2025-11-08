84c874b23711da9ca5918e40d3e93103a080c478
riya9818 <reeyabasnet999@gmail.com>
2025-11-08 21:29:11 +0545

    add player in views

--- teams/views.py
+++ teams/views.py
@@ -41,3 +41,20 @@ def team_detail(request, team_id):
     team = get_object_or_404(Team, id=team_id)
     players = Player.objects.all()
     return render(request, 'teams/team_detail.html', {'team': team, 'players': players})
+
+def add_player(request, team_id):
+    team = get_object_or_404(Team, id=team_id)
+    if request.method == 'POST':
+        player_id = request.POST.get('player_id')
+        new_player_name = request.POST.get('new_player_name')
+
+        if player_id:
+            player = Player.objects.get(id=player_id)
+        elif new_player_name:
+            player = Player.objects.create(name=new_player_name)
+        else:
+            return redirect('team_detail', team_id=team.id)
+
+        team.members.add(player)
+        team.save()
+        return redirect('team_detail', team_id=team.id)
