5bf9afe56ccf3a264331d4665dcaab4d97b6ec02
riya9818 <reeyabasnet999@gmail.com>
2025-11-08 21:26:08 +0545

    updated models to have players

--- teams/models.py
+++ teams/models.py
@@ -1,10 +1,20 @@
+# teams/models.py
 from django.db import models
 from django.contrib.auth.models import User
 
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    position = models.CharField(max_length=50, blank=True)
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Team(models.Model):
     name = models.CharField(max_length=100)
-    captain = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='captained_teams')
-    players = models.ManyToManyField(User, related_name='teams', blank=True)
+    captain = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, blank=True, related_name='captain_of')
+    members = models.ManyToManyField(Player, related_name='teams')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
