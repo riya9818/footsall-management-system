6c753cc75d56fa76aa2f62181398bc01006ea1cc
riya9818 <reeyabasnet999@gmail.com>
2025-11-09 21:01:03 +0545

    add player basic template

--- teams/urls.py
+++ teams/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('players/', views.player_list, name='player_list'),
+    path('players/add/', views.add_player, name='add_player'),
     path('', views.team_list, name='team_list'),
-    path('add/', views.team_create, name='team_create'),
-    path('edit/<int:pk>/', views.team_edit, name='team_edit'),
-    path('delete/<int:pk>/', views.team_delete, name='team_delete'),
-     path('<int:team_id>/', views.team_detail, name='team_detail'),
-    path('<int:team_id>/add_player/', views.add_player, name='add_player'),
+    path('add/', views.add_team, name='add_team'),
 ]
+
