96a49e3c19ea075b7a872bc4e32e1204713e8cc2
riya9818 <reeyabasnet999@gmail.com>
2025-11-10 23:37:14 +0545

    updated teams/urls.py

--- teams/urls.py
+++ teams/urls.py
@@ -1,14 +1,19 @@
-# teams/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.team_list, name='team_list'),
-    path('add/', views.add_team, name='add_team'),
-    path('<int:team_id>/', views.team_detail, name='team_detail'),
-    path('<int:team_id>/add_player/', views.add_player_to_team, name='add_player_to_team'),
-    path('<int:team_id>/remove_player/<int:player_id>/', views.remove_player_from_team, name='remove_player_from_team'),
-
+    # Player URLs
     path('players/', views.player_list, name='player_list'),
     path('players/add/', views.add_player, name='add_player'),
+    path('players/edit/<int:player_id>/', views.edit_player, name='edit_player'),
+    path('players/delete/<int:player_id>/', views.delete_player, name='delete_player'),
+
+    # Team URLs
+    path('teams/', views.team_list, name='team_list'),
+    path('teams/add/', views.add_team, name='add_team'),
+    path('teams/<int:team_id>/', views.team_detail, name='team_detail'),
+    path('teams/edit/<int:team_id>/', views.edit_team, name='edit_team'),
+    path('teams/delete/<int:team_id>/', views.delete_team, name='delete_team'),
+    path('teams/<int:team_id>/add_player/', views.add_player_to_team, name='add_player_to_team'),
+    path('teams/<int:team_id>/remove_player/<int:player_id>/', views.remove_player_from_team, name='remove_player_from_team'),
 ]
