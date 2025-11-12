7b0b78890d30e82af23ad556672d233bafd506e6
riya9818 <reeyabasnet999@gmail.com>
2025-11-12 21:07:38 +0545

    urls updated to fixed manage bookings

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.booking_list, name='booking_list'),
     path('book/', views.book_match, name='book_match'),
+     path('manage/', views.manage_bookings, name='manage_bookings'),
 ]
