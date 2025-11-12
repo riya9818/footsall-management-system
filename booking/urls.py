4933989e1babf16f1b3e42d146f9ac1e3ad0b7ba
riya9818 <reeyabasnet999@gmail.com>
2025-11-12 21:08:21 +0545

    fixed updated booking

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.booking_list, name='booking_list'),
     path('book/', views.book_match, name='book_match'),
-     path('manage/', views.manage_bookings, name='manage_bookings'),
+    path('manage/', views.manage_bookings, name='manage_bookings'),
+    path('update/<int:booking_id>/<str:new_status>/', views.update_booking_status, name='update_booking_status'),
 ]
