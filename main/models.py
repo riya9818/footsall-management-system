11c8ec670477baef4a26221657b31d6ee850b573
riya9818 <reeyabasnet999@gmail.com>
2025-11-14 21:54:28 +0545

    match models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Match(models.Model):
+    home_team = models.ForeignKey(Team, related_name="home_matches", on_delete=models.CASCADE)
+    away_team = models.ForeignKey(Team, related_name="away_matches", on_delete=models.CASCADE)
+    match_date = models.DateField()
+    match_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
