94c8a65e7f38261f62b3da205e9ee942f0aaf8b7
riya9818 <reeyabasnet999@gmail.com>
2025-11-14 21:58:30 +0545

    add match in views

--- main/views.py
+++ main/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def add_match(request):
+    if request.method == 'POST':
+        form = MatchForm(request.POST)
+        if form.is_valid():
+            match = form.save(commit=False)
+            
