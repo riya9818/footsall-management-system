e13d725eb029c0923075a8fad63421fe27adab99
riya9818 <reeyabasnet999@gmail.com>
2025-11-15 21:38:13 +0545

    match result forms

--- main/forms.py
+++ main/forms.py
@@ -4,3 +4,10 @@ class MatchForm(forms.ModelForm):
     class Meta:
         model = Match
         fields = ['home_team', 'away_team', 'match_date', 'match_time']
+
+
+
+class MatchResultForm(forms.ModelForm):
+    class Meta:
+        model = MatchResult
+        fields = ['home_score', 'away_score', 'man_of_the_match']
