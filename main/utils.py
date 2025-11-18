90c065ab2242a4309054facd2655b83a57e3ec30
riya9818 <reeyabasnet999@gmail.com>
2025-11-18 20:59:02 +0545

    try catch block to skip match with no result

--- main/utils.py
+++ main/utils.py
@@ -2,3 +2,9 @@ from .models import Match, MatchResult
 
 def calculate_standings():
     standings = {}
+
+    for match in Match.objects.all():
+        try:
+            result = match.matchresult
+        except MatchResult.DoesNotExist:
+            continue  # Skip matches with no result
