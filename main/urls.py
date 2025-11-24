1dbca10b43397b79bc3ec645ffc76b185c31a394
riya9818 <reeyabasnet999@gmail.com>
2025-11-24 14:14:10 +0545

    add player stats urls

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('standings/', league_standings, name='standings'),
+    path('match/<int:match_id>/stats/', add_player_stats, name='add_player_stats'),
 
 ]
