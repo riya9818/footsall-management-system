2b2a81a33ac60de727c471852af220464b3bcfc7
riya9818 <reeyabasnet999@gmail.com>
2025-12-02 23:50:56 +0545

    fixed league standings

--- main/urls.py
+++ main/urls.py
@@ -7,8 +7,8 @@ from .views import league_standings
 urlpatterns = [
     path('', views.home, name='home'),
     path('standings/', views.league_standings, name='standings'),
-    path('match/<int:match_id>/stats/', add_player_stats, name='add_player_stats'),
-    path('match/<int:match_id>/availability/', mark_availability, name='mark_availability'),
+    path('match/<int:match_id>/stats/', views.add_player_stats, name='add_player_stats'),
+    path('match/<int:match_id>/availability/',mark_availability, name='mark_availability'),
     path('match/<int:match_id>/attendance/', take_attendance, name='take_attendance'),
 
 ]
