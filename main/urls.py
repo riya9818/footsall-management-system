a34da797df4dbe7278f2b62d34da58962e4de7bb
riya9818 <reeyabasnet999@gmail.com>
2025-12-03 21:53:01 +0545

    removed duplicate

--- main/urls.py
+++ main/urls.py
@@ -3,15 +3,13 @@
 from django.urls import path
 from . import views
 
-from .views import league_standings
+from .views import league_standings, add_player_stats, take_attendance
 from teams.views import mark_availability
-
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('standings/', views.league_standings, name='standings'),
-    path('match/<int:match_id>/stats/', views.add_player_stats, name='add_player_stats'),
-    path('match/<int:match_id>/availability/',mark_availability, name='mark_availability'),
+    path('standings/', league_standings, name='standings'),
+    path('match/<int:match_id>/stats/', add_player_stats, name='add_player_stats'),
+    path('match/<int:match_id>/availability/', mark_availability, name='mark_availability'),
     path('match/<int:match_id>/attendance/', take_attendance, name='take_attendance'),
-
 ]
